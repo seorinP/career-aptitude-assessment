0ffe46402f287afe351d50307a48cf5769e5c642
Seorin <seorinpark96@gmail.com>
2021-11-26 13:43:25 +0900

    검사 예시 페이지 하단 공백 제거

--- src/pages/TestExample/index.js
+++ src/pages/TestExample/index.js
@@ -1,23 +1,33 @@
 import React from "react";
+import styled from 'styled-components';
+
 import Question from "../Test/Question";
 import ProgressBar from "../../components/ProgressBarComponent";
 import PageMoveButton from '../../components/PageMoveButtonComponent';
 
 
+const Title = styled.div`
+    font-family:'Binggrae-Bold';
+    font-size:1.5rem;
+    text-align:center;
+    color:#6B3FA0;
+    margin-top:1.9rem;
+    margin-bottom:6.4rem;
+`
+
 
 function TestExample({ setSelectedValue, initialValue, disabled, pageIndex, progressPercentage, onClick1, onClick2 }) {
     return (
-        pageIndex === 1 && (
             <>
               <div className="mb-4">
                 <ProgressBar text={'검사 예시'} percentage={progressPercentage} />
               </div>
 
-              <h4 className="mb-4">
+              <Title>
                 직업과 관련된 두개의 가치 중에서 자기에게 더 중요한 가치에
                 표시하세요.
-              </h4>
-  
+              </Title>
+        
               <Question
                 qitemNo={999}
                 question="두 개 가치 중에 자신에게 더 중요한 가치를 선택하세요."
@@ -31,14 +41,14 @@ function TestExample({ setSelectedValue, initialValue, disabled, pageIndex, prog
                 }}
               />
   
-              <div className="d-flex justify-content-between">
+              
+              <div className="button d-flex justify-content-between">
                 {/* 이전 다음 버튼 */}
-                <PageMoveButton type={'previous'} text={'처음 화면으로'} onClick={onClick1} />
+                <PageMoveButton className='button' type={'previous'} text={'처음 화면으로'} onClick={onClick1} />
                 &nbsp;
-                <PageMoveButton type={'next'} disabled={disabled} text={'검사 시작'} onClick={onClick2} />
+                <PageMoveButton className='button' type={'next'} disabled={disabled} text={'검사 시작'} onClick={onClick2} />
               </div>
             </>
-          )
     );
 }
 
